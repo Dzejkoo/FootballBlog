6c384c21e797fdef59e641a15b19412906a52b41
Jakub Jóźwiak <jozwiak.ja@icloud.com>
2022-01-25 22:24:10 +0100

    segregate players by positions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
-import { PlayerCard } from './components/moleculas/PlayerCard';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './assets/styles/theme';
 import { GlobalStyle } from './assets/styles/globalStyle';
+import { PlayerCard } from './components/moleculas/PlayerCard';
 
 const Wrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
 `;
+
+const cases = {
+  Goalkeeper: 'Goalkeeper',
+  Defender: 'Defender',
+  Midfielder: 'Midfielder',
+  Forward: 'Forward',
+};
 
 export const App = () => {
   const [players, setPlayers] = useState([]);
@@ -20,13 +27,37 @@ export const App = () => {
       .catch((err) => console.log(err));
   }, []);
 
+  const goalkeeper = players.filter((player) => player.position === cases.Goalkeeper);
+  const defender = players.filter((player) => player.position === cases.Defender);
+  const midfielder = players.filter((player) => player.position === cases.Midfielder);
+  const forward = players.filter((player) => player.position === cases.Forward);
+
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Wrapper>
-          {players.map((playerData, i) => (
-            <PlayerCard key={i} playerData={playerData}></PlayerCard>
+          <h2>{cases.Goalkeeper}</h2>
+          {goalkeeper.map((playerData, i) => (
+            <PlayerCard key={i} playerData={playerData} />
+          ))}
+        </Wrapper>
+        <Wrapper>
+          <h2>{cases.Defender}</h2>
+          {defender.map((playerData, i) => (
+            <PlayerCard key={i} playerData={playerData} />
+          ))}
+        </Wrapper>
+        <Wrapper>
+          <h2>{cases.Midfielder}</h2>
+          {midfielder.map((playerData, i) => (
+            <PlayerCard key={i} playerData={playerData} />
+          ))}
+        </Wrapper>
+        <Wrapper>
+          <h2>{cases.Forward}</h2>
+          {forward.map((playerData, i) => (
+            <PlayerCard key={i} playerData={playerData} />
           ))}
         </Wrapper>
       </ThemeProvider>
