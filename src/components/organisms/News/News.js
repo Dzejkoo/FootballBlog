1400ca6a89416fc5d4176a6be0168bce8d8ecf84
Jakub Jóźwiak <jozwiak.ja@icloud.com>
2022-03-01 21:13:43 +0100

    Read information from API by useReducer

--- src/components/organisms/News/News.js
+++ src/components/organisms/News/News.js
@@ -40,8 +40,56 @@ const query = `{
             }
           }`;
 
+const initialState = {
+  loading: false,
+  error: null,
+  articles: {},
+};
+
+const ACTION = {
+  CALL_API: 'call-api',
+  SUCCESS: 'success',
+  ERROR: 'error',
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTION.CALL_API: {
+      return {
+        ...state,
+        loading: true,
+      };
+    }
+    case ACTION.SUCCESS: {
+      return {
+        ...state,
+        loading: false,
+        articles: action.data,
+      };
+    }
+    case ACTION.ERROR: {
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
 export const News = () => {
+  const [state, dispath] = useReducer(reducer, initialState);
+  const {
+    loading,
+    error,
+    articles: { data },
+  } = state;
+  console.log(data);
+
   useEffect(() => {
+    dispath({ type: ACTION.CALL_API });
     axios
       .post(
         'https://graphql.datocms.com/',
@@ -54,23 +102,30 @@ export const News = () => {
           },
         }
       )
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
-  });
+      .then((response) => dispath({ type: ACTION.SUCCESS, data: response }))
+      .catch((error) => dispath({ type: ACTION.ERROR, error: error }));
+  }, []);
 
   return (
     <Wrapper>
-      <Plock nColumns={2}>
-        <InjuryReport />
-        <TransferNews />
-        <TeamNews />
-        <InjuryReport />
-        <TransferNews />
-        <TeamNews />
-        <InjuryReport />
-        <TransferNews />
-        <TeamNews />
-      </Plock>
+      {/* {console.log(data)} */}
+      {loading ? (
+        <p>loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <Plock nColumns={2}>
+          <InjuryReport />
+          <TransferNews />
+          <TeamNews />
+          <InjuryReport />
+          <TransferNews />
+          <TeamNews />
+          <InjuryReport />
+          <TransferNews />
+          <TeamNews />
+        </Plock>
+      )}
     </Wrapper>
   );
 };
